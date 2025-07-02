46e75ea0ce943bc02470f1e47501e1f5ed4540ed
Kalkidan-dev <cakek433@gmail.com>
2025-07-02 11:49:51 +0300

    Edited

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM information_schema.columns
-WHERE table_schema = DATABASE()
-  AND table_name = 'books';
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
